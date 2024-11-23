2054c929c8b9a79aab430fe6ad54d5a97a663469
Tyuiu.DikanovAA@outlook.com <Tyuiu.DikanovAA@outlook.com>
2024-11-23 13:53:15 +0500

    Add project files.

---
+++ Tyuiu.DikanovAA.Sprint4.Task0.V18.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+using Tyuiu.DikanovAA.Sprint4.Task0.V18.Lib;
+namespace Tyuiu.DikanovAA.Sprint4.Task0.V18.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int[] array = { 9, 8, 7, 6, 5, 7, 3, 2, 7, 3 };
+
+            int wait = 41;
+            int res = ds.GetSumOddArrEl(array);
+
+            Assert.AreEqual(wait, res);
+
+        }
+    }
+}
